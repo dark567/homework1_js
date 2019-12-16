035139154f381d6076088d2424801adfb5b8e5cd
dark567 <darkmit@list.ru>
2019-12-16 18:35:25 +0200

    20191216_05

---
+++ 2/task2_4.js
@@ -0,0 +1,26 @@
+/*Переделайте приведенный код так, чтобы в нем использовались операции +=, -=, *=, /=, ++, --. Количество строк кода при этом не должно измениться. Код для переделки: 
+var num = 1;
+num = num + 12;
+num = num - 14;
+num = num * 5;
+num = num / 7;
+num = num + 1;
+num = num - 1;
+alert(num);*/
+
+
+var num = 1;
+console.log(`num ="${num}"`);
+num += 12;
+console.log(`num ="${num}"`);
+num -= 14;
+console.log(`num ="${num}"`);
+num *= 5;
+console.log(`num ="${num}"`);
+num /= 7;
+console.log(`num ="${num}"`);
+num++;
+console.log(`num ="${num}"`);
+num--;
+console.log(`num ="${num}"`);
+alert(num);
